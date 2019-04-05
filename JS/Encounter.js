88096abfda9906a6169602ad87cb89c6f893679b
HAJ523 <HAJ.523@gmail.com>
2019-04-05 16:44:09 -0500

    Start encounters.

---
+++ JS/Encounter.js
@@ -0,0 +1,25 @@
+/*
+  Encounter map halding, lighting, visiion.
+
+  Author: HAJ523
+  2019-04 Created.
+*/
+
+var EN = {};
+
+EN.initEncounter = function() {
+  EN.M = {};
+  //EN.M.T = {}; //Tile & Color //Can be read from map & Objects?
+  EN.M.O = {}; //Occlusion
+  EN.M.L = {}; //Lighting
+  EN.M.W = {}; //Walkable
+}
+
+EN.updateEncounterCanvas = function() {
+  //Update the layers based on current object properties.
+  //Occlusion
+  //Light
+  //Walkable
+  //Update the display based on the layers
+
+}
