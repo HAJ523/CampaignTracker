2d64c2a0f70a7cb45572172ae5cf705c8acbd2c9
HAJ523 <HAJ.523@gmail.com>
2019-04-19 16:00:23 -0500

    Continue encounter UI.

--- JS/Encounter.js
+++ JS/Encounter.js
@@ -12,13 +12,14 @@ EN.initEncounter = function() {
 }
 
 EN.addOC = function() {
-  
+
 }
 
 EN.updateEncounterCanvas = function() {
   //Update the layers based on current object properties.
   //Occlusion
   //Light
+    //If r*3 < map perimeter use circle with distance radius for outside points instead of perimeter points!
   //Walkable
   //Update the display based on the layers
 
