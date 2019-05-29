4e78f2dd93104f2f5424ed2d0d19f1af4e06587c
HAJ523 <HAJ.523@gmail.com>
2019-05-29 16:04:16 -0500

    Make adjustments to listeners and continue one the encounter changes.

--- JS/Encounter.js
+++ JS/Encounter.js
@@ -9,10 +9,33 @@ var EN = {};
 
 EN.initEncounter = function() {
 
+  //Add Event Listeners to the canvas.
 }
 
 EN.addOC = function() {
 
+}
+
+EN.resetEncounterCanvas = function() {
+  //Clear the canvas and reset the dimensions.
+  MR.CV.width = data.pages[data.slctPage].M.W * MR.FS + MR.BR*2; //Add 10px on all sides of the display.
+  MR.CV.height = data.pages[data.slctPage].M.H * MR.FS + MR.BR*2;
+
+  //Draw the background first
+  MR.CX.fillStyle = data.pages[data.slctPage].M.C;
+  MR.CX.fillRect(0,0,MR.CV.width, MR.CV.height);
+
+  //Setup the font for printing to the canvas.
+  MR.CX.font = MR.FS + "px Square";
+  MR.CX.textAlign = "left";
+  MR.CX.textBaseline = "top";
+
+  EN.updateEncounterCanvas();
+}
+
+EN.closeEncounter = function() {
+  //Remove the event listeners to prevent conflict with mapper.
+  //TODO Add event listeners.
 }
 
 EN.updateEncounterCanvas = function() {
