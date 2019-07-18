fecd56623b90176844f2f2d9ec3ca619f851a254
HAJ523 <HAJ.523@gmail.com>
2019-07-18 09:14:15 -0500

    Only prevent default when performing function.

--- JS/Keyboard.js
+++ JS/Keyboard.js
@@ -31,30 +31,35 @@ KB.keyUD = function(e) {
   console.log(key);
 
   switch(key) {
+    case 16: //Shift
     case 17: //Control
-    //case 18: //Alt
+    case 18: //Alt
       KB.modKeys[key] = (e.type == 'keydown');
       break;
     default:
+      if (KB.modKeys[16]) break;
+      if (KB.modKeys[18]) break;
       if (KB.modKeys[17] && e.type == 'keydown') {
         switch (key) {
           case 80:
-            console.log("new page!");
             CT.prompt(CT.newPage,'New Page','Enter the full page path and name:','Page Path','Ex. \'Parent/Sub Parent/Page Name\'');
+            e.preventDefault();
             break;
           case 74:
           case 77:
           case 69:
             CT.changeView(String.fromCharCode(key));
+            e.preventDefault();
             break;
           case 83:
             CT.saveData();
+            e.preventDefault();
             break;
           case 76:
             CT.prompt(CT.loadData,'Load Campaign','What campaign do you wish to load?','Campaign','Campaign Name');
+            e.preventDefault();
             break;
         }
-        e.preventDefault();
       }
   }
 }
@@ -92,55 +97,71 @@ KB.markdownShortcut = function(e) {
       break;
   }
 
+  if (KB.modKeys[16]) return;
+  if (KB.modKeys[18]) return;
   if (KB.modKeys[17]) { //Control + Key
     switch(key) {
       case 49: //1
         JL.headerText(e.currentTarget, 1);
+        e.preventDefault();
         break;
       case 50: //2
         JL.headerText(e.currentTarget, 2);
+        e.preventDefault();
         break;
       case 51: //3
         JL.headerText(e.currentTarget, 3);
+        e.preventDefault();
         break;
       case 52: //4
         JL.headerText(e.currentTarget, 4);
+        e.preventDefault();
         break;
       case 53: //5
         JL.headerText(e.currentTarget, 5);
+        e.preventDefault();
         break;
       case 54: //6
         JL.headerText(e.currentTarget, 6);
+        e.preventDefault();
         break;
       case 66: //B
         JL.boldText(e.currentTarget);
+        e.preventDefault();
         break;
       case 73: //I
         JL.italicText(e.currentTarget);
+        e.preventDefault();
         break;
       case 85: //U
         JL.underlineText(e.currentTarget);
+        e.preventDefault();
         break;
       case 82: //R
         JL.strikethroughText(e.currentTarget);
+        e.preventDefault();
         break;
       case 75: //K //Ordered List
         JL.listText(e.currentTarget, "+");
+        e.preventDefault();
         break;
       case 76: //L //Unordered List
         JL.listText(e.currentTarget, "*");
+        e.preventDefault();
         break;
       case 72: //H
         JL.horizontalRule(e.currentTarget);
+        e.preventDefault();
         break;
       case 65: //A
         JL.insertTable(e.currentTarget, 3, 3);
+        e.preventDefault();
         break;
       case 81: //Q //Blockqoute
         JL.blockquoteText(e.currentTarget);
+        e.preventDefault();
         break;
     }
-    e.preventDefault();
   }
 }
 
